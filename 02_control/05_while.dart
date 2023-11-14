66300281538dc436b27b81684826d33420ee9c6b
Luis A Morla Sánchez <luislabertmsanchez@gmail.com>
2023-11-14 18:36:57 -0400

    update 05_while.dart

--- 02_control/05_while.dart
+++ 02_control/05_while.dart
@@ -1,5 +1,20 @@
 import 'dart:io';
 
 void main(List<String> args) {
+
+  
+  String continuar = 'y';
+  int contador = 0;
+
+  while ( continuar == 'y' ) {
+
+    contador++;
+    stdout.writeln('Contador: $contador');
+
+    stdout.writeln('desea continuar? (y/n)');
+    continuar = stdin.readLineSync() ?? 'n';
+    
+
+  }
   
 }
