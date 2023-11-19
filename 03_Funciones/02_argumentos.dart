171f421f06b8a426fa5f0bb566e71a9d0048d369
Luis A Morla Sánchez <luislabertmsanchez@gmail.com>
2023-11-19 00:33:09 -0400

    update 02_argumentos.dart

--- 03_Funciones/02_argumentos.dart
+++ 03_Funciones/02_argumentos.dart
@@ -1,13 +1,44 @@
 
-void saludar ( String mensaje ,[String nombre = 'Insertar Nombre', int? poder = 26] ){
+void saludar ( String mensaje ,[String nombre = 'Insertar Nombre', int? edad = 1] ){
 
 
-  print('$mensaje $nombre >>$poder<<');
+  print('$mensaje $nombre >>$edad<<');
 
 }
 
+/* Para pasar de argumentos posicionales a argumentos por nombre void saludar
+ void saludar ( String mensaje ,[String nombre = 'Insertar Nombre', int? edad = 1] ) 
+ void saludar ( {  String mensaje ,[String nombre = 'Insertar Nombre', int? edad = 1] })
+ */
+
+
+void saludar2(
+
+
+  // Argumento posicional
+  String? mensaje,
+  
+  
+  {
+   // Agumento por nombre (es requerido)
+  required String  nombre,
+
+  // Argumento Opcional
+  int? edad = 2
+
+}){
+
+  // Cuerpo de la función
+  print('Saludar2: $mensaje $nombre - $edad');
+
+}
+
+
+
+
 void main(List<String> args) {
 
-  saludar('La energia', 'Es recuperada' );
+  saludar('Tony', 'Stark' );
+  saludar2('Super Hero', nombre: 'Pepper');
   
 }
