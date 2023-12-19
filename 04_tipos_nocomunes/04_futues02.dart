999a4f219426f517b51d2ecccbf898e7f5f919e2
Luis A Morla Sánchez <luislabertmsanchez@gmail.com>
2023-12-19 07:26:59 -0400

    Update en Futures

---
+++ 04_tipos_nocomunes/04_futues02.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  
+
+
+File file  = new File(Directory.current.path + '\\04_tipos_nocomunes\\assets\\personas.txt');
+
+
+Future<String> f = file.readAsString();
+
+
+
+f.then()
+
+print('Fin del main');
+
+
+
+} 
